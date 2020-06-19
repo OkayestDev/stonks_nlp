9cdc6a8ee9ba9f0443df7528c03b5a76ee801e8d
Kyle Richardson <richardson.a.kyle.95@gmail.com>
2020-06-19 13:59:35 -0400

    Changing to predict()

--- src/stonks_nlp.py
+++ src/stonks_nlp.py
@@ -3,5 +3,5 @@ from src.model_loader import load_model
 model = load_model()
 
 def get_sentiment_of_tweet(tweet):
-    result = model.pipe(tweet)
+    result = model.predict(tweet)
     return result
