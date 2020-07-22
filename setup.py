d611e0bf11e39e19f7f0d3fb246beb14884db0a0
Kyle Richardson <kyle.richardson@homeeondemand.com>
2020-07-22 16:13:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name="stonks_nlp",  # Replace with your own username
+    name="stonks_nlp",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
